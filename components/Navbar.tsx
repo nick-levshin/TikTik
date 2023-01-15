c1c210711ff7b6c785b1608d7c0bdcf8c0a98fba
Nick Levshin <nikita.levshin2016@gmail.com>
2023-01-15 11:54:51 +0300

    feat: add google auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 
 import Logo from '../utils/tiktik-logo.png';
+import { createOrGetUser } from '../utils';
 
 const Navbar = () => {
+  const [user, setUser] = useState(false);
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -17,6 +19,19 @@ const Navbar = () => {
           <Image className="cursor-pointer" src={Logo} alt="TikTik" />
         </div>
       </Link>
+
+      <div className="">SEARCH</div>
+
+      <div>
+        {user ? (
+          <div className="">Logged In</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => createOrGetUser(response)}
+            onError={() => console.log('Error')}
+          />
+        )}
+      </div>
     </div>
   );
 };
